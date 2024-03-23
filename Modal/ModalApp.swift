c039d01489190c08b065202a3b819535475c62cc
aciosui <mrana.swe@gmail.com>
2024-03-23 11:34:04 -0400

    Initial Commit

---
+++ Modal/ModalApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModalApp.swift
+//  Modal
+//
+//  Created by MD  on 3/23/24.
+//
+
+import SwiftUI
+
+@main
+struct ModalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
